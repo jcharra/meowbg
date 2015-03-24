39db1f9e8c62021855ac05e2f47e078092f7b664
unknown <CharraJ@HGDECLFR001262.grp.haufemg.com>
2015-03-24 09:11:32 +0100

    create logdir if necessary

--- meowbg.py
+++ meowbg.py
@@ -6,6 +6,9 @@ BASE = os.path.dirname(__file__)
 sys.path.insert(0, BASE)
 
 os.environ["MEOWBG_ROOT"] = BASE
+logdir = os.path.join(BASE, "logs")
+if not os.path.exists(logdir):
+	os.mkdir(logdir)
 
 from meowbg.gui.main import BoardApp
 
